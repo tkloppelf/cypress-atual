1e9fae93c0ac010d55250f0f661b2ed104b03391
unknown <tkloppelfilho@gmail.com>
2025-09-19 10:06:49 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,17 +1,24 @@
 describe('Orange HRM Tests', () => {
+
+const selectorList = {
+  usernameField: '[name="username"]',
+  passwordField: '[name="password"]',
+  loginButton: '.oxd-button'
+}
+
   it('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains("Dashboard")
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('test')
-    cy.get('[name="password"]').type('test')
-    cy.get('.oxd-button').click()
+    cy.get(selectorList.usernameField).type('test')
+    cy.get(selectorList.passwordField).type('test')
+    cy.get(selectorList.loginButton).click()
     cy.get('.oxd-alert')
   })
 })
